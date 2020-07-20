6460d353e5119cb5b14a9d74ee22a8c2c60600af
Nivesh Saharan <nivesh@gmail.com>
2020-07-20 17:08:11 +0530

    Add temperature conversions

---
+++ src/Temperature.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Centire\UnitConversions;
+
+/**
+ * Class Temperature
+ *
+ * @package \Centire\UnitConversions
+ */
+class Temperature
+{
+    private float $celsius;
+
+    public static function fromCelsius(float $celsius): self
+    {
+        return new static($celsius);
+    }
+
+    public function __construct(float $celsius)
+    {
+        $this->celsius = $celsius;
+    }
+
+    public function toFahrenheit(): float
+    {
+        return ($this->celsius * 1.8) + 32;
+    }
+}
